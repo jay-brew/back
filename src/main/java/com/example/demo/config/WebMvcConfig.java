44b38fd30fa8dc11e6a4683bf85f08eb4f2f532f
jay-brew <brew0@naver.com>
2023-06-02 18:55:10 +0900

    notebook - aws front domain add

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer{
         // 모든 경로에 대해
         registry.addMapping("/**")
             // Origin이 http:localhost:3000인 경우
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("http://localhost:3000", "http://prod-todo-react-service.ap-northeast-2.elasticbeanstalk.com", "https://api.brewamericano.com")
             //GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 활용한다.(허용)
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")    // 모든 헤더 허용
