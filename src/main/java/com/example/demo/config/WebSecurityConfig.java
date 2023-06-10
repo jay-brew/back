5ab6e72703de316e79bbcf296df26dc02c89ff5d
jay-brew <brew0@naver.com>
2023-06-11 03:26:02 +0900

    notebook - backend social login finish

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,12 +1,17 @@
 package com.example.demo.config;
 
 import com.example.demo.security.JwtAuthenticationFilter;
+import com.example.demo.security.OAuthSuccessHandler;
+import com.example.demo.security.OAuthUserServiceImpl;
+import com.example.demo.security.RedirectUrlCookieFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
@@ -14,6 +19,12 @@ import org.springframework.web.filter.CorsFilter;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
+    @Autowired
+    private OAuthUserServiceImpl oAuthUserService; // 우리가 만든 OAuthUserServiceImpl 추가
+    @Autowired
+    private OAuthSuccessHandler oAuthSuccessHandler; // Success Handler 추가
+    @Autowired
+    private RedirectUrlCookieFilter redirectUrlFilter;
 
     @Override
     protected  void configure(HttpSecurity http) throws Exception {
@@ -21,16 +32,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors() // WebMvcConfig 에서 이미 설정했으므로 기본 cors 설정
             .and()
             .csrf() // csrf 는 현재 사용하지 않으므로 disable
-                .disable()
+            .disable()
             .httpBasic()    // 토큰을 사용하므로 basic 인증 disabled
-                .disable()
-            .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .disable()
+            .sessionManagement()    // session 기반 아님을 선언
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .authorizeRequests()    // /와 /auth/** 경로는 인증 안 해도 됨.
-                .antMatchers("/", "/auth/**").permitAll()
+            .antMatchers("/", "/auth/**", "/oauth2/**").permitAll() // 엔드포인트 추가
             .anyRequest()   // /와 /auth/** 이외의 모든 경로는 인증해야됨.
-                .authenticated();
+            .authenticated()
+            .and()
+            .oauth2Login() // oauth2Login 설정
+            .redirectionEndpoint()
+            .baseUri("/oauth2/callback/*")
+            .and()
+            .authorizationEndpoint()
+            .baseUri("/auth/authorize")   // OAuth 2.0 흐름 시작을 위한 엔드포인트 추가
+            .and()
+            .userInfoEndpoint()
+            .userService(oAuthUserService)  // OAuthUserServiceImpl를 유저 서비스로 등록
+            .and()
+            .successHandler(oAuthSuccessHandler)    // Success Handler 등록
+            .and()
+            .exceptionHandling()
+            .authenticationEntryPoint(new Http403ForbiddenEntryPoint());    // Http403ForbiddenEntryPoint 추가
 
         // filter 등록.
         // 매 요청마다
@@ -42,6 +68,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // CorsFilter.class 추가 시 반드시 org.springframework.web.filter.CorsFilter 를 import 해야 한다.
                 // 만약 스프링 시큐리티 필터 로그에서 JwtAuthenticationFilter 를 찾을 수 없다면, import 경로를 재확인하라.
         );
+        http.addFilterBefore(   // before
+                redirectUrlFilter,
+                OAuth2AuthorizationRequestRedirectFilter.class  // 리다이렉트 되기 전에 필터를 실행
+        );  
 
         // 마지막 addFilterAfter() 메서드를 실행하는 것은
         // JwtAuthenticationFilter 를 CorsFilter 이후에 실행하라고 설정하는 것이다.
--- src/main/java/com/example/demo/security/TokenProvider.java
+++ src/main/java/com/example/demo/security/TokenProvider.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -14,37 +15,24 @@ import java.util.Date;
 @Slf4j
 @Service
 public class TokenProvider {
-    private static final String SECRET_KEY = "FLQDRdTrldafSRKDFDFVJvndrkTUDRIENVKndpjtinitTDUIOPnioDRJTtad5kjKL4K3djfVjd4JK5RJ";
+    private static final String SECRET_KEY = "N3EKNFNkAEKASDdfDFDATKEJQMPdojeP3O3KAJCODJ13MKFNMCK3OEPDOVNAIEP3kd3FDCI3AIF3Zg";
 
     // JWT 라이브러리를 이용해 JWT 토큰을 생성한다.
     public String create(UserEntity userEntity){
         // 기한 지금으로부터 1일로 설정
         Date expiryDate = Date.from(
-                Instant.now()
-                        .plus(1, ChronoUnit.DAYS));
-        /*
-        {   // header
-            "alg":"HS512"
-        }.
-        {   // payload
-        "sub": "351341d31341c13414t1244",
-        "iss": "demo app",
-        "iat": 1421315,
-        "exp": 1435243,
-        }.
-        // SECRET_KEY를 이용해 서명한 부분
-        N3EKNFNkAEKASDdfDFDATKEJQMPdojeP3O3KAJCODJ13MKFNMCK3OEPDOVNAIEP3kd3FDCI3AIF3Zg
-        */
+            Instant.now()
+                    .plus(1, ChronoUnit.DAYS));
         // JWT Token 생성
         return Jwts.builder()
-                // header에 들어갈 내용 및 서명을 하기 위한 SECRET_KEY
-                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
-                // payload에 들어갈 내용
-                .setSubject(userEntity.getId()) // sub
-                .setIssuer("demo app")      // iss
-                .setIssuedAt(new Date())    // iat
-                .setExpiration(expiryDate)  // exp
-                .compact();
+            // header에 들어갈 내용 및 서명을 하기 위한 SECRET_KEY
+            .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+            // payload에 들어갈 내용
+            .setSubject(userEntity.getId()) // sub
+            .setIssuer("demo app")      // iss
+            .setIssuedAt(new Date())    // iat
+            .setExpiration(expiryDate)  // exp
+            .compact();
     }
 
     // 토큰을 디코딩, 파싱 및 위조여부를 확인한다. 이후에 우리가 원하는 subject 즉 유저의 아이디를 리턴한다.
@@ -55,10 +43,24 @@ public class TokenProvider {
         // 위조되지 않았다면 페이로드(Claims) 리턴, 위조라면 예외를 날림
         // 그 중 우리는 userId가 필요하므로 getBody를 부른다.
         Claims claims = Jwts.parser()
-                .setSigningKey(SECRET_KEY)
-                .parseClaimsJws(token)
-                .getBody();
+            .setSigningKey(SECRET_KEY)
+            .parseClaimsJws(token)
+            .getBody();
 
         return claims.getSubject();
+    }
+
+    public String createUser(final Authentication authentication){
+        ApplicationOAuth2User userPrincipal = (ApplicationOAuth2User) authentication.getPrincipal();
+        Date expiryDate = Date.from(
+            Instant.now()
+                .plus(1, ChronoUnit.DAYS));
+
+        return Jwts.builder()
+            .setSubject(userPrincipal.getName()) // id가 리턴됨.
+            .setIssuedAt(new Date())
+            .setExpiration(expiryDate)
+            .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+            .compact();
     }
 }
